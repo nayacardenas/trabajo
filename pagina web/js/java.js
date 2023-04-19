6f1520ff4820e4da39a3df438c475d2f82533718
nayacardenas <107703943+nayacardenas@users.noreply.github.com>
2023-04-19 17:12:26 -0400

    Add files via upload

---
+++ pagina web/js/java.js
@@ -0,0 +1,14 @@
+var carritoVisible= false;
+
+
+if(document.readyState=='loading'){
+    document.addEventListener('DOMContentLoaded',ready)
+
+}else{
+    ready();
+}
+
+function ready(){
+    var botonesEliminarItem = document.getElementsByClassName('btn-eliminar');
+    for(vari=0; i< botonesEliminarItem.length);
+}
